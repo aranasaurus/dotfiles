86b358aa0f8c10f49c616558f7b1346993e4ea22
Ryan Arana <ryan.arana@hey.com>
2024-07-11 09:14:52 -0700

    Update plugins and then disable the auto plugin update prompt

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -47,8 +47,6 @@ require("lazy").setup({
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
 	install = { colorscheme = { "tokyonight", "catppuccin" } },
-	-- automatically check for plugin updates
-	checker = { enabled = true },
 	ui = {
 		-- If you are using a Nerd Font: set icons to an empty table which will use the
 		-- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
