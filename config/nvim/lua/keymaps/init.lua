7afc0a13240004843522f6a02480572de3045091
Ryan Arana <ryan.arana@hey.com>
2024-07-11 09:15:23 -0700

    Add floaterm toggle mapping

--- config/nvim/lua/keymaps/init.lua
+++ config/nvim/lua/keymaps/init.lua
@@ -28,4 +28,6 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
-vim.keymap.set("n", "<leader>gg", ":FloatermNew --width=0.9 --height=0.9 lazygit<cr>", { desc = "Open lazygit" })
+-- Floaterm
+vim.keymap.set("n", "<leader>lg", ":FloatermNew --width=0.9 --height=0.9 lazygit<cr>", { desc = "[l]azy[g]it" })
+vim.keymap.set("n", "<leader>tt", ":FloatermToggle<cr>", { desc = "[T]oggle a floating [T]erminal" })
